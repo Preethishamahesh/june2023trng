931ca64695234c7886efdec7830caf354c5c520b
Preethishamahesh <131242507+Preethishamahesh@users.noreply.github.com>
2023-06-07 16:52:11 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class PalindromeNumber{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n;
+int rd=0;
+System.out.println("Enter any positive integer");
+n=sc.nextInt();
+int num=n;
+while(n>0){
+rd=rd*10+(n%10);
+n=n/10;
+}
+if(num==rd)
+{
+System.out.println("Palindrome");
+}
+else
+{
+System.out.println("Not palindrome");
+}
+}
+}
