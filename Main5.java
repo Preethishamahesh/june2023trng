d020bc149ca79cf1d46b1182ad72c51312210c93
Preethishamahesh <131242507+Preethishamahesh@users.noreply.github.com>
2023-06-14 14:08:16 +0530

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Main5{
+public static void main(String args[]){
+ArrayList<Float>marks=new ArrayList<>();
+marks.add(70.0F);
+marks.add(90.0F);
+marks.add(80.0F);
+System.out.println("ArrayList:"+marks);
+}
+}
---
+++ Main8.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Main8{
+public static void main(String args[]){
+ArrayList<String>languages=new ArrayList<>();
+languages.add("Java");
+languages.add("Python");
+languages.add("Swift");
+System.out.println("ArrayList:"+languages);
+}
+}
